a4fcbf236139eabb3097b4f2076fb58aeebfd642
solphilos <trichosirius@gmail.com>
2022-03-29 21:09:59 -0400

    new functions

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// creates 256 divs and appends them to the parent div class 'container'
+// creates 256 divs and appends them to the parent div class 'container'. Adds id to each, numbered 1-256.
 makeDivs = () => {
-    for ( let i = 0; i < 256; i++) {
-        const container = document.querySelector('.container');
+    for ( let i = 1; i < 257; i++) {
+        const container = document.querySelector('#container');
         const div = document.createElement('div');
         container.appendChild(div);
-        div.classList.add('grid');
+        div.setAttribute('id', 'grid' + i);  
     }
+    addListen();
+}
+     
+//////////////////////////////////////////////////////////////////////////////////////////////
+   //  adds event listener to container
+
+function addListen() {
+    document.querySelector('#container').addEventListener('mouseover', getIdName);
+}
+
+  // when mouseover event enters element, identify id name ('grid1', 'grid2', etc.)
+
+function getIdName() {
+     let name = document.querySelectorAll(':hover')
+     let array = Array.from(name);
+     let idName = array.pop();
+     console.log(idName)
+
 }
 
 
-// make function that makes hover color remain active when mouse leaves the element
-
-function leaveActive() {
-  const click = document.getElementsByClassName('.grid').addEventListener('click');
-  
 
 
-
-    
-}
-
-
-
-
-
-
-
-
-
+// run functions
 makeDivs();
-
-
